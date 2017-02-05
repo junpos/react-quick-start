04faad515b2faad1dfbb680ba4ea8482368326a0
Jun Kim <junpos@gmail.com>
2017-02-05 00:50:30 -0500

    change folder structure

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import App from './components/App';
-import Reducers from './reducers';
+import App from './app/components/App';
+import Reducers from './app/reducers';
 
 const store = createStore(Reducers);
 const rootElement = document.getElementById('app');
